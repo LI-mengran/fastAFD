d2c0aa7a1cfd2a9b1416450eb383e8d606b7b4df
LI-mengran <59810866+LI-mengran@users.noreply.github.com>
2024-07-16 19:29:42 +0800

    Delete out directory

--- out/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-java -jar -Xms2g -Xmx52g FastAFD.jar -r -1 -f ./dataset/restaurant.csv -t 0 -s threshold/restaurant.txt -e ""
-java -jar -Xms2g -Xmx52g FastAFD.jar -r -1 -f ./dataset/pcm.csv -t 0 -s threshold/pcm.txt -e ""
-java -jar -Xms2g -Xmx52g FastAFD.jar -r -1 -f ./dataset/struct.csv -t 0 -s threshold/struct.txt -e ""
-
-
